13839dc8afc608a264d0177250dc6a76aad7e016
Arielito123 <104540300+Arielito123@users.noreply.github.com>
2023-11-03 16:51:28 -0300

    Palindromo
    para saber si una palabra es palindroma o no con metodos y sin metodos

---
+++ Palindromo(Metodo)/Palindromo(Metodo)/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromo_Metodo_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+            int longitud;
+            bool palindromo = true;
+
+            Console.Write("Ingrese una palabra: ");
+            palabra = Console.ReadLine();//Parseo el string
+            palabra = palabra.ToUpperInvariant();//String a mayuscula para que no interfiera en el ingreso
+            longitud = palabra.Length;//Guardo el n° de la palabra en longitud
+
+            palindromo = Program.Palindromo(longitud, palabra);//CUANOD LO LLAMO AL METODO, LO QUE DEVUELVA SE VA A GUARDAR EN ESA NUEVA VARIABLE.
+
+            if (palindromo == true)//Determina que opcion mostrar.
+            {
+                Console.WriteLine("La palabra " + palabra + " es un palindromo. ");
+            }
+            else
+            {
+                Console.WriteLine("La palabra " + palabra + " no es un palindromo. ");
+            }
+            Console.ReadKey();//una pausa
+        }
+
+
+        static bool Palindromo(int largo, string pal)
+        {
+            bool PALIND = true;
+            int CONTADOR = 0;
+
+            for (int i = largo - 1; i >= 0; i--)
+            {
+                if (pal[i] != pal[CONTADOR])
+                {
+                    PALIND = false; // Si se cumple la condicion, vale false
+                    break;
+
+                }
+                CONTADOR++;
+            }
+            return PALIND;
+
+        }
+
+    }
+}
---
+++ palindromo/palindromo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;//variable de tipo string
+            int longitud,cont = 0;//variable de tipo entera que contendra la longitud, y un contador
+            bool palindromo = true;
+            
+            Console.Write(" Ingrese una palabra ");//leyenda
+            palabra = Console.ReadLine();//parseamos el string
+            palabra = palabra.ToUpperInvariant();//convertimos el string a mayuscula
+            longitud=palabra.Length;//guaramos la longitud de la palabra
+
+
+            for (int i = longitud - 1; i >= 0; i--) //esto pregunta que i es igual a longitud en la posicion menos 1 que seria la ultima posicion 
+            {  // que mientras indice no sea menor o igual a 0 
+                //indice decrementa hasta llegar a la posicion 0
+
+                if (palabra[i]!=palabra[cont]) 
+                {
+                    palindromo = false; // igual cambia a falso
+                    break;//corta el ciclo de repeticion
+ 
+                }
+                cont++;//incrementa la posicion de cont
+ 
+            }
+
+            if (palindromo == true) //paldrindomo es verdadero entonces habra encontrado una coincidencia en la palabra de adelante al reves
+            {
+                Console.WriteLine(" La Palabra es palindromo "); //leyenda diciendo q es palindromo
+            }
+            else //de lo contrario
+            {
+                Console.WriteLine(" La palabra no es palindromo ");//la palabra no es palindromo
+            }
+
+
+
+            Console.ReadKey();//pausa
+        
+
+        }
+    }
+}
